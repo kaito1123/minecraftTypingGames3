3b2093800f5c3962b0895892879cfdea9310c06a
kaito1123 <78860399+kaito1123@users.noreply.github.com>
2022-01-09 17:32:34 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 'use strict';
 
 {
+  const target = document.getElementById('target');
+
   function setWord() {
     word = words.splice(Math.floor(Math.random() * words.length), 1)[0];
     target.textContent = word;
@@ -14,14 +16,16 @@
     'COMMAND',
     'SKELETONRIDER',
   ];
+
   let word;
   let loc = 0;
   let startTime;
   let isPlaying;
 
-  const target = document.getElementById('target');
-
   document.addEventListener('click', () => {
+    const disabled = document.getElementById('disabled');
+    disabled.remove();
+    
     if (isPlaying === true) {
       return;
     }
